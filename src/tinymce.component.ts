45edf75a9ce993bb3701628c2b195cc79e59d81a
Timmy Garrabrant <timmy.garrabrant@gmail.com>
2016-10-26 13:39:24 -0700

    update

--- src/tinymce.component.ts
+++ src/tinymce.component.ts
@@ -1,13 +1,13 @@
 import {
     Component,
-    OnDestroy,
-    AfterViewInit,
     EventEmitter,
     forwardRef,
     Input,
-    Output
+    Output,
+    NgZone,
+    AfterContentInit, OnChanges
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 declare var tinymce;
 
 @Component({
@@ -19,26 +19,64 @@ declare var tinymce;
             multi: true
         }
     ],
-    template: `<textarea  id="{{elementId}}"></textarea>`
+    template: `<textarea id="{{elementId}}">{{initVal}}</textarea>`
 })
-export class TinymceComponent implements AfterViewInit, OnDestroy {
+export class TinymceComponent implements ControlValueAccessor {
     elementId: String = 'host';
-    @Output() onEditorKeyup = new EventEmitter<any>();
+
+    @Output() change = new EventEmitter();
+    @Output() ready = new EventEmitter();
+    @Output() blur = new EventEmitter();
+
+    @Input() initVal;
+
     _value = '';
+    zone;
     editor;
 
     ngAfterViewInit() {
         tinymce.init({
             selector: '#' + this.elementId,
-            plugins: [],
+            plugins: ['link', 'paste', 'table', 'autoresize'],
             skin_url: 'assets/skins/lightgray',
+            autoresize_overflow_padding: 0,
             setup: editor => {
                 this.editor = editor;
                 editor.on('keyup', () => {
                     const content = editor.getContent();
-                    this.onEditorKeyup.emit(content);
+                    this.updateValue(content);
                 });
             },
+        });
+    }
+
+    /**
+    * Constructor
+    */
+    constructor(zone: NgZone) {
+        console.log(this.value)
+        this.value = this.initVal;
+        console.log(this.value)
+        this.zone = zone;
+    }
+
+    get value(): any { return this._value; };
+    @Input() set value(v) {
+        if (v !== this._value) {
+            this._value = v;
+            this.onChange(v);
+        }
+    }
+
+    /**
+    * Value update process
+    */
+    updateValue(value) {
+        this.zone.run(() => {
+            this.value = value;
+            this.onChange(value);
+            this.onTouched();
+            this.change.emit(value);
         });
     }
 
